bfcf507786260098c1ed6e13ebc842c712855179
Enedin9000 <jan1987@telenet.be>
2022-12-02 10:34:04 +0100

    Renamed BLU functions, added drain function (yet to rewire), used existing systemStrength table

--- scripts/globals/spells/blue/digest.lua
+++ scripts/globals/spells/blue/digest.lua
@@ -24,26 +24,23 @@ end
 
 spellObject.onSpellCast = function(caster, target, spell)
 
-    local dmg = 5 + 0.575 * caster:getSkillLevel(xi.skill.BLUE_MAGIC)
+
     --get resist multiplier (1x if no resist)
     local params = {}
     params.ecosystem = xi.ecosystem.AMORPH
-    params.diff = caster:getStat(xi.mod.MND)-target:getStat(xi.mod.MND)
-    params.attribute = xi.mod.MND
+    params.attackType = xi.attackType.MAGICAL
+    params.damageType = xi.damageType.DARK
+    params.diff = 0 -- no stat increases magic accuracy
     params.skillType = xi.skill.BLUE_MAGIC
-    params.bonus = 1.0
-    local resist = applyResistance(caster, target, spell, params)
-    --get the resisted damage
-    dmg = dmg * resist
-    --add on bonuses (staff/day/weather/jas/mab/etc all go in this function)
-    dmg = addBonuses(caster, spell, target, dmg)
-    --add in target adjustment
-    dmg = adjustForTarget(target, dmg, spell:getElement())
-    --add in final adjustments
+    params.dmgMultiplier = 5
 
-    if dmg < 0 then
-        dmg = 0
-    end
+    local dmg = 5 * math.floor(caster:getSkillLevel(xi.skill.BLUE_MAGIC) * 0.11)
+    local resist = applyResistance(caster, target, spell, params)
+    dmg = dmg * resist
+    dmg = addBonuses(caster, spell, target, dmg)
+    dmg = adjustForTarget(target, dmg, spell:getElement())
+
+    if dmg < 0 then dmg = 0 end
 
     if target:isUndead() then
         spell:setMsg(xi.msg.basic.MAGIC_NO_EFFECT)
@@ -54,9 +51,8 @@ spellObject.onSpellCast = function(caster, target, spell)
         dmg = target:getHP()
     end
 
-    params.attackType = xi.attackType.MAGICAL
-    params.damageType = xi.damageType.DARK
-    dmg = BlueFinalAdjustments(caster, target, spell, dmg, params)
+
+    dmg = blueFinalizeDamage(caster, target, spell, dmg, params)
     caster:addHP(dmg)
 
     return dmg
--- scripts/globals/spells/blue/regurgitation.lua
+++ scripts/globals/spells/blue/regurgitation.lua
@@ -41,11 +41,11 @@ spellObject.onSpellCast = function(caster, target, spell)
     params.attribute = xi.mod.INT
     params.skillType = xi.skill.BLUE_MAGIC
     params.bonus = 1.0
-    local damage = BlueMagicalSpell(caster, target, spell, params, INT_BASED)
+    local damage = blueDoMagicalSpell(caster, target, spell, params, INT_BASED)
     if caster:isBehind(target, 15) then -- guesstimating the angle at 15 degrees here
         damage = math.floor(damage * 1.25)
     end
-    damage = BlueFinalAdjustments(caster, target, spell, damage, params)
+    damage = blueFinalizeDamage(caster, target, spell, damage, params)
 
     --TODO: Knockback? Where does that get handled? How much knockback does it have?
 
